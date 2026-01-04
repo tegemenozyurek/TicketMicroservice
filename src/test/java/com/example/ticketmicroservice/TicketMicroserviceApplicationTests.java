9a88a1a6223850b697a17c51546eb8fbfe27f432
PC <egemenozyurek@outlook.com>
2026-01-04 21:25:24 +0300

    initial

---
+++ src/test/java/com/example/ticketmicroservice/TicketMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticketmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
