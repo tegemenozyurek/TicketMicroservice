242061c214e2170b37c15263284ecf793315a1e3
spikepiece <oguzerenacar@gmail.com>
2026-01-04 21:15:10 +0100

    ticketreply crud

---
+++ src/main/java/com/example/ticketmicroservice/TicketRepliesController.java
@@ -0,0 +1,42 @@
+package com.example.ticketmicroservice;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ticketreplies")
+public class TicketRepliesController {
+    private final TicketRepliesService ticketRepliesService;
+
+    public TicketRepliesController(TicketRepliesService ticketRepliesService) {
+        this.ticketRepliesService = ticketRepliesService;
+    }
+
+    @GetMapping
+    public List<TicketReplies> getAllTicketReplies() {
+        return ticketRepliesService.getAllTicketReplies();
+    }
+
+    @GetMapping(path = "{id}")
+    public TicketReplies getTicketRepliesById(@PathVariable Long id) {
+        return ticketRepliesService.getTicketRepliesById(id);
+    }
+
+    @PostMapping("/save")
+    public TicketReplies saveTicketReplies(@RequestBody TicketReplies ticketReplies) {
+        return ticketRepliesService.saveTicketReplies(ticketReplies);
+    }
+
+    @PutMapping("/updateTicketReplies/{id}")
+    public TicketReplies updateTicketReplies(@PathVariable Long id, @RequestBody TicketReplies ticketReplies) {
+        ticketReplies.setId(id);
+        return ticketRepliesService.updateTicketReplies(ticketReplies);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void deleteTicketReplies(@PathVariable Long id) {
+        ticketRepliesService.deleteTicketReplies(id);
+    }
+}
